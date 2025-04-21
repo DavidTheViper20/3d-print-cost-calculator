5aa0fe83184476d97aa83d5ffafb224b33534d8c
David Naguib <david.theviper@hotmail.com>
2025-04-22 01:21:22 +1000

    Updated Python app with latest changes

--- app.py
+++ app.py
@@ -125,5 +125,5 @@ def download_file(filename):
         return "File not found", 404
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
